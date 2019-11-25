f1d5fe74a8a50025e9a07d2d8570a84bad16fb96
Dylan Stephano-Shachter <dshachter@nasuni.com>
2019-11-25 16:33:37 -0500

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='optprompt',
-    version='0.1.0',
+    version='0.2.0',
     author='Dylan Stephano-Shachter',
     author_email='dylan@theone.ninja',
     description='A prompting option parser',
